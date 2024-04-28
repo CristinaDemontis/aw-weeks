b8df78c074f11f20fd7d47a97ea47da7876f5ba3
CristinaDemontis <s281766@studenti.polito.it>
2024-04-28 11:34:02 +0200

    appunti 22/04

--- week06/public/API.js
+++ week06/public/API.js
@@ -6,10 +6,14 @@ import { Answer } from "./QAModels.js";
 
 const URL = "http://localhost:3001/api";
 
-async function getAllAnswers(questionId) {
+// trasformiamo una funzione che probablmente bloccherebbe il codice in una async function, cosi da non creare problemi 
+async function getAllAnswers(questionId) { // è una funzione async perchè non voglio bloccare il codice 
     // call  /api/questions/:id/answers
+
+    // dobbiamo predendere il promise, prendere il body, prendere le info nl body, e restituire la lista di risposte
+
     const response =  await fetch(URL+`/questions/${questionId}/answers`);
-    const answers = await response.json();
+    const answers = await response.json(); // ricorda che la response è un json che ritorna una promise 
 if (response.ok) {
     return answers.map((e) => new Answer(e.id, e.text, e.respondent, e.score, e.date, e.questionId) );
 } else {
